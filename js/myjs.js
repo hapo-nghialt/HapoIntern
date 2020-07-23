b70ec65daadeaecfbb1439fb8881de81b153fa74
hapo-nghialt <nghia.lt@haposoft.com>
2020-07-23 16:51:32 +0700

    edit banner, icon footer

--- js/myjs.js
+++ js/myjs.js
@@ -5,10 +5,6 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("comment");
